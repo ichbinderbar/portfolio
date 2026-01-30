bb2966a631bdf227eaa49b891418dfc8a28be90f
Juan <idrovel@gmail.com>
2026-01-30 11:31:16 -0500

    Debug with alert instead of console.log

--- src/scripts/payments.js
+++ src/scripts/payments.js
@@ -1,13 +1,8 @@
 // Payments page initialization
-console.log('[DEBUG] payments.js loading...');
-console.log('[DEBUG] PAYPHONE_STORE_ID from env:', process.env.PAYPHONE_STORE_ID);
+alert('STORE_ID: ' + process.env.PAYPHONE_STORE_ID);
 
 import initI18n from "./i18n";
 import initPaymentSection from "./payphone";
 
-console.log('[DEBUG] imports done, initializing...');
-
 initI18n();
 initPaymentSection();
-
-console.log('[DEBUG] initialization complete');
